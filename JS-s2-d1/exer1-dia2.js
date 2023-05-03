7899b7a9e9291a1d7c84352731acadd729eba5c2
Kainã Monteiro <kainamonteiro@gmail.com>
2023-05-03 19:58:46 -0300

    exer1-dia2

---
+++ JS-s2-d1/exer1-dia2.js
@@ -0,0 +1,63 @@
+//Parte1//
+
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 0; index < numbers.length; index += 1) {
+  console.log(numbers[index]);
+} */
+
+//Parte 2//
+
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  result += numbers[index];
+}
+
+console.log(result); */
+
+//Parte 3//
+
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  soma += numbers[index];
+}
+
+let media = soma / numbers.length;
+console.log(media); */
+
+//parte 4//
+
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  result += numbers[index];
+}
+
+result = result / numbers.length;
+
+if (result > 20) {
+  console.log('O valor da média aritmética é maior que 20');
+} else {
+  console.log('O valor da média aritmética é menor ou igual a 20');
+} */
+
+//Parte 5//
+
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numeroMaior = numbers [0];
+
+for (let index = 1; index < numbers.length; index += 1) {
+  if(numbers[index] > numeroMaior) {
+    numeroMaior = numbers[index]
+  }
+}
+
+console.log(numeroMaior); */
+
+//Parte 6//
+
